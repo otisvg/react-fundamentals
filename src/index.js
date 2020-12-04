91ef685e588961c72d0fe018cedf1e6cd87d4a83
Otis <otisvickersgraver@gmail.com>
2020-12-04 00:09:12 +0000

    render a simple greeting on localhost

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React  from 'react'
+import ReactDOM from 'react-dom' 
+
+function Greeting() {
+  return <h1>Hey, I'm Otis :)</h1>
+}
+
+ReactDOM.render(<Greeting/>, document.getElementById('root')) 
