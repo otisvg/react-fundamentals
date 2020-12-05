1c9c4ab1b006a84fcbfae4771e26fb29de6670c7
Otis <otisvickersgraver@gmail.com>
2020-12-05 16:02:38 +0000

    map over game list, rendering game data using props

--- src/index.js
+++ src/index.js
@@ -2,44 +2,68 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "./index.css";
+const games = [
+  {
+    img: "https://m.media-amazon.com/images/I/61DAQGX2QML._AC_UY218_.jpg",
+    title: "Harvest Moon: Friends of Mineral Town",
+    date: "2006",
+  },
 
-const firstGame = {
-  img: "https://m.media-amazon.com/images/I/61DAQGX2QML._AC_UY218_.jpg",
-  title: "Harvest Moon: Friends of Mineral Town",
-  date: "2006",
-};
-
-const secondGame = {
-  img:
-    "https://upload.wikimedia.org/wikipedia/en/4/47/PlayStation_Fighting_Force_Cover.jpg",
-  title: "Fighting Force",
-  date: "1997",
-};
+  {
+    img:
+      "https://upload.wikimedia.org/wikipedia/en/4/47/PlayStation_Fighting_Force_Cover.jpg",
+    title: "Fighting Force",
+    date: "1997",
+  },
+  {
+    img:
+      "https://www.mobygames.com/images/covers/l/236048-disney-s-hercules-playstation-front-cover.jpg",
+    title: "Hercules",
+    date: "1997",
+  },
+  {
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/51ah%2BeKF%2BhL._AC_.jpg",
+    title: "Rayman",
+    date: "1995",
+  },
+  {
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/51YSBXMJ6AL._AC_SX466_.jpg",
+    title: "Tarzan",
+    date: "1999",
+  },
+  {
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/81Y-PwNlBzL._AC_SL1500_.jpg",
+    title: "Crash Bandicoot 2: The Wrath of Cortex",
+    date: "1997",
+  },
+  {
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/51YSBXMJ6AL._AC_SX466_.jpg",
+    title: "Spyro: Year of the Dragon",
+    date: "2001",
+  },
+];
 
 function GameList() {
   return (
     <section className="gamelist">
-      <Game
-        img={firstGame.img}
-        title={firstGame.title}
-        date={firstGame.date}
-      ></Game>
-      <Game
-        img={secondGame.img}
-        title={secondGame.title}
-        date={secondGame.date}
-      ></Game>
+      {games.map((game) => {
+        return <Game game={game}></Game>;
+      })}
     </section>
   );
 }
 
-const Game = ({ img, title, date, owner }) => {
+const Game = (props) => {
+  const { img, title, date } = props.game;
   return (
     <article className="game">
       <img src={img} alt="" />
       <h4>{title}</h4>
       <h6>Release date: {date}</h6>
-      <p>{owner}</p>
     </article>
   );
 };
