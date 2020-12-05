e0509817bb01f13beecff6ea9d135b691d852572
Otis <otisvickersgraver@gmail.com>
2020-12-05 00:22:22 +0000

    index.js setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-import React  from 'react'
-import ReactDOM from 'react-dom' 
+import React from "react";
+import ReactDOM from "react-dom";
+
+//stateless functional component
+//need to always return some jsx
 
 function Greeting() {
-  return <h1>Hey, I'm Otis :)</h1>
+  return <h1>Hey, I'm Otis :)</h1>;
 }
 
-ReactDOM.render(<Greeting/>, document.getElementById('root')) 
+ReactDOM.render(<Greeting />, document.getElementById("root"));
