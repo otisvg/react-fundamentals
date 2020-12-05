914fa035113b06540b56346ababeabbed1a3a187
Otis <otisvickersgraver@gmail.com>
2020-12-05 14:26:05 +0000

    using props to render two separate games

--- src/index.js
+++ src/index.js
@@ -1,56 +1,44 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
 import "./index.css";
+
+const firstGame = {
+  img: "https://m.media-amazon.com/images/I/61DAQGX2QML._AC_UY218_.jpg",
+  title: "Harvest Moon: Friends of Mineral Town",
+  date: "2006",
+};
+
+const secondGame = {
+  img:
+    "https://upload.wikimedia.org/wikipedia/en/4/47/PlayStation_Fighting_Force_Cover.jpg",
+  title: "Fighting Force",
+  date: "1997",
+};
 
 function GameList() {
   return (
     <section className="gamelist">
-      <Game />
-      <Game />
-      <Game />
-      <Game />
-      <Game />
-      <Game />
-      <Game />
-      <Game />
-      <Game />
-      <Game />
-      <Game />
-      <Game />
-      <Game />
-      <Game />
-      <Game />
-      <Game />
-      <Game />
-      <Game />
-      <Game />
-      <Game />
-      <Game />
-      <Game />
+      <Game img={firstGame.img} title={firstGame.title} date={firstGame.date} />
+      <Game
+        img={secondGame.img}
+        title={secondGame.title}
+        date={secondGame.date}
+      />
     </section>
   );
 }
 
-const Game = () => {
+const Game = (props) => {
+  console.log(props);
   return (
     <article className="game">
-      <Image />
-      <Title />
+      <img src={props.img} alt="" />
+      <h4>{props.title}</h4>
+      <h6>Release date: {props.date}</h6>
+      <p>{props.owner}</p>
     </article>
   );
-};
-
-const Image = () => {
-  return (
-    <img
-      src="https://m.media-amazon.com/images/I/61DAQGX2QML._AC_UY218_.jpg"
-      alt=""
-    />
-  );
-};
-
-const Title = () => {
-  return <p>Harvest Moon: Friends of Mineral Town</p>;
 };
 
 ReactDOM.render(<GameList />, document.getElementById("root"));
